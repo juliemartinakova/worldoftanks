4d266627f3e53ec8b2b039fad0e2dc25677c0124
Julie Martináková <julie-martinakova.wotonline@gmail.com>
2024-11-13 16:13:35 +0100

    Fixing game's assets loading #4

--- public/scripts/gui.js
+++ public/scripts/gui.js
@@ -1385,7 +1385,7 @@ localizationElements = document.querySelectorAll("[data-locale]")
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('src/scripts/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then((registration) => {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             })
