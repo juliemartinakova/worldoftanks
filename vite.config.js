80310666e4ce4fa6f675c32ffe30c46c44402c36
Julie Martináková <julie-martinakova.wotonline@gmail.com>
2024-11-13 15:53:08 +0100

    Fixed game's icons loading #2

--- vite.config.js
+++ vite.config.js
@@ -14,19 +14,19 @@ export default defineConfig({
       theme_color: '#111111',
 
       icons: [{
-        src: 'gui/game/pwa-64x64.png',
+        src: 'assets/gui/game/pwa-64x64.png',
         sizes: '64x64',
         type: 'image/png',
       }, {
-        src: 'pwa-192x192.png',
+        src: 'assets/gui/game/pwa-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       }, {
-        src: 'gui/game/pwa-512x512.png',
+        src: 'assets/gui/game/pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       }, {
-        src: 'gui/game/maskable-icon-512x512.png',
+        src: 'assets/gui/game/maskable-icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
